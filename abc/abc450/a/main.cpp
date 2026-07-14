6e598cd23b9e9c217cbdcf2fc2cbba758dbbb76b
s2422114 <s2422114@stu.musashino-u.ac.jp>
2026-07-14 23:17:05 +0900

    ABC450 A: AC (N から 1 までカンマ区切りで出力)

---
+++ abc/abc450/a/main.cpp
@@ -0,0 +1,23 @@
+#define _GLIBCXX_DEBUG
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    while (N > 1) {
+        cout << N << ",";
+        N--;
+        
+    }
+    
+    cout << "1" << endl;
+}
