24ed41fab5abe62667dfbf2f09074dd6526c7c5b
s2422114 <s2422114@stu.musashino-u.ac.jp>
2026-08-12 02:21:11 +0900

    ABC460 B: AC (2円の共有点判定、2乗比較で平方根回避)

--- abc/abc460/b/main.cpp
+++ abc/abc460/b/main.cpp
@@ -12,5 +12,29 @@
 using namespace std;
 
 int main() {
-    
+    int T;
+    cin >> T;
+    for(int i = 1; i <= T; i++) {
+        bool check = false;
+        long long X1, Y1, R1, X2, Y2, R2;
+        cin >> X1 >> Y1 >> R1 >> X2 >> Y2 >> R2;
+        // 共有点を持つか判定
+        long long d2 = (X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1);
+        if (d2 == (R1 + R2) * (R1 + R2) ) {
+            check = true;
+        } else if ((R1 - R2) * (R1 - R2) < d2 && d2 < (R1 + R2) * (R1 + R2)) {
+            check = true;
+        } else if (d2 == (R1 - R2) * (R1 - R2)) {
+            check = true;
+        }
+
+        if (check)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
 }
