d268bf29aa248ee58cd047097aed58fe36ac66ee
s2422114 <s2422114@stu.musashino-u.ac.jp>
2026-09-12 01:05:18 +0900

    tessoku A18: AC (部分和問題、二次元DP)

---
+++ tessoku/a18/main.cpp
@@ -0,0 +1,44 @@
+#define _GLIBCXX_DEBUG
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+int main() {
+    int N, S, A[69];
+    bool dp[69][10009];
+
+    // 入力
+    cin >> N >> S;
+    for (int i = 1; i <= N; i++) cin >> A[i];
+
+    // dp（i = 0）
+    dp[0][0] = true;
+    for (int i = 1; i <= S; i++) dp[0][i] = false;
+
+    // dp (i >= 1)
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= S; j++) {
+            if (j < A[i]) {
+                if (dp[i-1][j] == true) dp[i][j] = true;
+                else dp[i][j] = false;
+            }
+            if (j >= A[i]) {
+                if (dp[i-1][j] == true || dp[i-1][j-A[i]] == true) dp[i][j] = true;
+                else dp[i][j] = false;
+            }
+        }
+    }
+
+    // 出力
+    if (dp[N][S] == true) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
