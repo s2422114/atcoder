c087c2545fa6ffd8c2b5128097a113a333d02eaa
s2422114 <s2422114@stu.musashino-u.ac.jp>
2026-09-15 23:53:23 +0900

    tessoku A29: AC (繰り返し二乗法でmod累乗)

---
+++ tessoku/a29/main.cpp
@@ -0,0 +1,31 @@
+#define _GLIBCXX_DEBUG
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+long long Power(long long a, long long b, long long m) {
+    long long p = a, Answer = 1;
+    for (int i = 0; i < 30; i++) {
+        int wari = (1 << i);
+        if ((b / wari) % 2 == 1) {
+            Answer = (Answer * p) % m;
+        }
+        p = (p * p) % m;
+    }
+    return Answer;
+}
+
+int main() {
+    long long a, b;
+    cin >> a >> b;
+    cout << Power(a, b, 1000000007) << endl;
+    return 0;
+}
